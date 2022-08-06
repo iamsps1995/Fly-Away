de7484c5bc161ce9650fee5ff19f8e62ce368344
iamsps1995 <105659734+iamsps1995@users.noreply.github.com>
2022-08-06 11:24:33 +0530

    Add files via upload

---
+++ Basic.java
@@ -0,0 +1,11 @@
+package com.myproject;
+
+public class Basic {
+	static int a = 10;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+            System.out.println(a);
+	}
+
+}
